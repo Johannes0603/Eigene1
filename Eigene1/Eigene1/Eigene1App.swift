6d5dd68fff3b31017aa36cf659f5c77e49873ef7
Johannes <Hannes.9090@gmx.de>
2023-11-22 05:38:13 +0100

    create
    init

---
+++ Eigene1/Eigene1/Eigene1App.swift
@@ -0,0 +1,17 @@
+//
+//  Eigene1App.swift
+//  Eigene1
+//
+//  Created by Johannes Ahlborn on 22.11.23.
+//
+
+import SwiftUI
+
+@main
+struct Eigene1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
